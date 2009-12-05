b3108f6bfa8435a98fd8c2d5f4119ebcfda6a9f0
Tammer Saleh <tsaleh@gmail.com>
2009-12-05 11:21:38 -0800

    Only showing 1 row of posts on front page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   skip_before_filter :require_user, :only => [:show]
 
   def show
-    @posts = Post.ordered.show_unpublished(logged_in?).to(7)
+    @posts = Post.ordered.show_unpublished(logged_in?).to(3)
   end
 end
