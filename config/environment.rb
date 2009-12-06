e293fb201daf2304fc8601055fc030921ac3387a
Tammer Saleh <tsaleh@gmail.com>
2009-12-06 13:30:07 -0800

    Updates for gemcutter

--- config/environment.rb
+++ config/environment.rb
@@ -12,13 +12,13 @@ load(heroku_env) if File.exists?(heroku_env)
 
 Rails::Initializer.run do |config|
   config.gem 'ambethia-smtp-tls', :lib => 'smtp-tls', :source => 'http://gems.github.com', :version => '>= 1.1.2'
-  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com', :version => '>= 2.3.11'
-  config.gem 'thoughtbot-paperclip', :lib => 'paperclip', :source => 'http://gems.github.com', :version => '>= 2.3.1'
-  config.gem 'josevalim-inherited_resources', :lib => 'inherited_resources', :source => 'http://gems.github.com', :version => '>= 0.8.5'
-  config.gem 'justinfrench-formtastic', :lib => 'formtastic', :source => 'http://gems.github.com', :version => '>= 0.2.2'
-  config.gem 'chrislloyd-gravtastic', :lib => 'gravtastic', :source => 'http://gems.github.com', :version => '>= 2.0.0'
-  config.gem 'authlogic', :version => '>= 2.1.1'
-  config.gem 'RedCloth'
+  config.gem 'will_paginate',       :version => '>= 2.3.11'
+  config.gem 'paperclip',           :version => '>= 2.3.1'
+  config.gem 'inherited_resources', :version => '>= 0.8.5'
+  config.gem 'formtastic',          :version => '>= 0.2.2'
+  config.gem 'gravtastic',          :version => '>= 2.0.0'
+  config.gem 'authlogic',           :version => '>= 2.1.1'
+  config.gem 'RedCloth',            :version => '>= 4.0.0'
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
