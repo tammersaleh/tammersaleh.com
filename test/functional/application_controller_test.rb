5cddcf182ad8b1458727ba0bb0d9a1d532e5cd58
Tammer Saleh <tsaleh@gmail.com>
2009-12-06 13:48:43 -0800

    functional test fix

--- test/functional/application_controller_test.rb
+++ test/functional/application_controller_test.rb
@@ -35,28 +35,18 @@ class ApplicationControllerTest < ActionController::TestCase
 
     context "getting a public page" do
       setup { get :unprotected_foo }
-
-      # should "not try to include the user's name" do
-      #   assert_no_match(/Hello/, @response.body)
-      # end
-
-      # should "not present a logout link" do
-      #   assert_no_match(/Logout/, @response.body)
-      # end
+      should "not present a logout link" do
+        assert_no_match(/logout/, @response.body)
+      end
     end
   end
 
   as_a_logged_in_user do
     context "the layout" do
       setup { get :foo }
-
-      # should "include the user's name" do
-      #   assert_match(/#{@logged_in_user.name}/, @response.body)
-      # end
-
-      # should "present the user with a logout link" do
-      #   assert_select 'a[href=?]', user_session_url, "Logout"
-      # end
+      should "present the user with a logout link" do
+        assert_select 'a[href=?]', logout_url, "Logout"
+      end
     end
   end
 end
