7b088bbd90803e781402a7d5d8e177d4a5913b9a
Tammer Saleh <tsaleh@gmail.com>
2009-12-06 15:33:57 -0800

    validating format of urls

--- test/unit/recommendation_test.rb
+++ test/unit/recommendation_test.rb
@@ -2,4 +2,16 @@ require 'test_helper'
 
 class RecommendationTest < ActiveSupport::TestCase
   should_validate_presence_of :quote, :who, :where, :where_url
+
+  %w(where_url company_url).each do |url|
+    should_allow_values_for url, 
+                            "http://google.com", 
+                            "https://g_oo-gle.ca", 
+                            "http://google.com/~some/path.asd"
+
+    should_not_allow_values_for url, 
+                                "ftp://google.com", 
+                                "eat me", 
+                                "http://google.com/%20you%20suck"
+  end
 end
