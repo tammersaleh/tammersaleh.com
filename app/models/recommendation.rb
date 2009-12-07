3900ab3c0f4fd29e490e7bc9342d3c8385dd0163
Tammer Saleh <tsaleh@gmail.com>
2009-12-06 16:29:58 -0800

    imported existing recommendations

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,5 +1,5 @@
 class Recommendation < ActiveRecord::Base
-  attr_accessible :quote, :who, :where, :where_url, :position, :company, :company_url
+  attr_accessible :quote, :who, :who_url, :where, :where_url, :position, :company, :company_url
   validates_presence_of :quote, :who, :where, :where_url
   validates_format_of :where_url,   :with => URL_REGEX, :allow_blank => true
   validates_format_of :company_url, :with => URL_REGEX, :allow_blank => true
