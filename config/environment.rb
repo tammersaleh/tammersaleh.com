9fac7bdc663ba9eb68dde23f6a0a60296ecb4ecf
Tammer Saleh <tsaleh@gmail.com>
2010-04-02 09:35:36 -0700

    Fixed test failures due to bad IH version

--- config/environment.rb
+++ config/environment.rb
@@ -14,7 +14,7 @@ Rails::Initializer.run do |config|
   config.gem 'ambethia-smtp-tls', :lib => 'smtp-tls', :source => 'http://gems.github.com', :version => '>= 1.1.2'
   config.gem 'will_paginate',       :version => '>= 2.3.11'
   config.gem 'paperclip',           :version => '>= 2.3.1'
-  config.gem 'inherited_resources', :version => '>= 0.8.5'
+  config.gem 'inherited_resources', :version => '~> 0.9.0'
   config.gem 'formtastic',          :version => '>= 0.2.2'
   config.gem 'gravtastic',          :version => '>= 2.0.0'
   config.gem 'authlogic',           :version => '>= 2.1.1'
