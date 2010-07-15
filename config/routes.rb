34181760ee40952d0bee4d1b01f1044c7d34b548
Tammer Saleh <tsaleh@gmail.com>
2010-07-15 16:58:02 -0400

    Removed front page.

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,5 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :assets
   map.resources :recommendations, :except => :show
 
-  map.root :controller => "home", :action => "show"
+  map.root :controller => "posts", :action => "index"
 end
