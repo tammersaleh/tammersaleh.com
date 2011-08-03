def1b0477887d5f8d8670945be2084d4c6824895
Tammer Saleh <tsaleh@gmail.com>
2011-08-02 18:53:55 -0700

    Finished image support

--- lib/tammer_saleh/my_red_cloth_template.rb
+++ lib/tammer_saleh/my_red_cloth_template.rb
@@ -1,13 +1,75 @@
 class MyRedClothTemplate < Tilt::RedClothTemplate
+  BASE_IMAGE_URL = "/attachments/images"
+  include Padrino::Helpers::TagHelpers
+
   def prepare
-    @engine = RedCloth.new(code_filter(data))
+    @engine = RedCloth.new(image_filter(code_filter(data)))
     @output = nil
   end
+
+  private
 
   def code_filter(txt)
     txt.gsub(/@@@ *(\w*)\r?\n? *(.+?)\r?\n?@@@/m) do
       klass = $1.present? ? " class=\"#{$1.downcase}\"" : ""
       "<pre><code#{klass}>#{$2}</code></pre>"
     end
+  end
+
+  def image_filter(txt)
+    txt.gsub(/^%([<>]?)([0-9a-zA-Z_.-]+)([^%]*)%/) do
+      float    = $1
+      image_id = $2
+      size     = $3
+
+      float = :right if float == '>'
+      float = :left  if float == '<'
+
+      uploaded_image_tag(image_id, :size => size, :float => float)
+    end
+  end
+
+  def uploaded_image_tag(image_name, opts = {})
+    opts.symbolize_keys
+
+    opts[:size] = "original" if opts[:size].blank?
+    opts[:size].strip!
+
+    basename, ext = image_name.split('.')
+    image_src = "#{BASE_IMAGE_URL}/#{basename}/#{opts[:size]}.#{ext}"
+    meta = meta_for_image(basename)
+
+    return "<div class='warning'>unknown image #{image_name}</div>" unless File.directory?(image_basedir(basename))
+
+    image_options = { :src => image_src }
+    image_options[:class] = "float-#{opts[:float]}" if opts[:float]
+    image_options[:alt]   = meta[:description] if meta[:description]
+
+    href = meta[:source_url]
+
+    img_tag = content_tag :img, "", image_options
+    if href
+      return "<a href='#{href}'>#{img_tag}</a>"
+    else
+      return img_tag
+    end
+  end
+
+  def image_basedir(basename)
+    File.join(TammerSaleh.settings.public, BASE_IMAGE_URL, basename)
+  end
+
+  def meta_for_image(basename)
+    file = File.join(image_basedir(basename), "meta.yml")
+    if File.exist?(file)
+      YAML.load_file(file).delete_if {|k, v| v.blank? }
+    else
+      {}
+    end
+  end
+
+  # Required to confuse padrino
+  def block_is_template?(_)
+    false
   end
 end
--- spec/image_filter_spec.rb
+++ spec/image_filter_spec.rb
@@ -1,16 +1,73 @@
 require "spec_helper"
 
-describe "given a textile template with an image link" do
-  before do
-    create_template("layouts/application.html.haml", "%h1 Layout\n= yield")
-    create_template("posts/bar.html.textile", "%file/large.jpg")
+describe "given a textile template with an image link " do
+  before { create_template("layouts/application.html.haml", "%h1 Layout\n= yield") }
+
+  describe "and an image link" do
+    before do
+      create_asset("attachments/images/file/original.jpg", "image data") 
+      create_template("posts/bar.html.textile", "%file.jpg large%") 
+    end
+
+    context "the rendered page" do
+      before { visit "/posts/bar" }
+      subject { page }
+
+      it { should have_selector('img[src="/attachments/images/file/large.jpg"]') }
+    end
   end
 
-  context "the rendered page" do
-    before { visit "/posts/bar" }
-    subject { page }
+  describe "and an image link with metadata" do
+    before do
+      create_template("posts/bar.html.textile", "%file.jpg%") 
+      create_asset("attachments/images/file/original.jpg", "image data") 
+      create_asset("attachments/images/file/meta.yml", <<-EOS) 
+                                                         --- 
+                                                         :description: Me Speaking
+                                                         :source_url: ""
+                                                       EOS
+    end
 
-    it { should have_selector('img[src="/attachments/images/file/large.jpg"]') }
+    context "the rendered page" do
+      before { visit "/posts/bar" }
+      subject { page }
+
+      it { should have_selector('img[alt="Me Speaking"]') }
+      it { should_not have_selector('a img') }
+    end
+  end
+
+  describe "and an image link with metadata that includes a url" do
+    before do
+      create_template("posts/bar.html.textile", "%file.jpg%") 
+      create_asset("attachments/images/file/original.jpg", "image data") 
+      create_asset("attachments/images/file/meta.yml", <<-EOS) 
+                                                         --- 
+                                                         :description: ""
+                                                         :source_url: http://google.com
+                                                       EOS
+    end
+
+    context "the rendered page" do
+      before { visit "/posts/bar" }
+      subject { page }
+
+      it { should_not have_selector('img[alt]') }
+      it { should have_selector('a[href="http://google.com"] img') }
+    end
+  end
+
+  describe "and an image link without a file" do
+    before { create_template("posts/bar.html.textile", "%bad_file.jpg%") }
+
+    context "the rendered page" do
+      before { visit "/posts/bar" }
+      subject { page }
+
+      it { should_not have_selector('img') }
+      it { should have_selector('.warning') }
+      it { page.source.should match("unknown") }
+    end
   end
 end
 
