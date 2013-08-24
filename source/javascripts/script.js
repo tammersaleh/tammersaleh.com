60abd9f1b3b397735857556f52c7d535e36b104f
Tammer Saleh <tsaleh@gmail.com>
2013-08-23 18:39:19 -0700

    Optimizing font loading

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -19,8 +19,20 @@
 //= require libs/bigtext.js
 //  require libs/jquery.fittext.js
 
-jQuery(window).load(function () {
-  $('#tammersaleh').bigtext();
-  $('#about_me').bigtext();
-});
+// jQuery(window).load(function () {
+//   $('#tammersaleh').bigtext();
+//   $('#about_me').bigtext();
+// });
+
+(function() {
+  var config = {
+    kitId: 'ilx3ybb',
+    scriptTimeout: 3000,
+    active: function () {
+      $('#tammersaleh').bigtext();
+      $('#about_me').bigtext();
+    }
+  };
+  var h=document.getElementsByTagName("html")[0];h.className+=" wf-loading";var t=setTimeout(function(){h.className=h.className.replace(/(\s|^)wf-loading(\s|$)/g," ");h.className+=" wf-inactive"},config.scriptTimeout);var tk=document.createElement("script"),d=false;tk.src='//use.typekit.net/'+config.kitId+'.js';tk.type="text/javascript";tk.async="true";tk.onload=tk.onreadystatechange=function(){var a=this.readyState;if(d||a&&a!="complete"&&a!="loaded")return;d=true;clearTimeout(t);try{Typekit.load(config)}catch(b){}};var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(tk,s)
+})();
 
